eaf62b850a99a3b36d9838283dd40f9cfd126a26
DiegoPerez1 <dieguitodieslow@gmail.com>
2023-05-26 12:00:31 -0300

    50% Orks page

---
+++ src/Components/WAAGH/WAAGH.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import KOPTERO from '../../assets/KOPTERO.png'
+import './WAAGH.css'
+import BadMoons from '../../assets/BadMoons.png'
+import BloodAxes from '../../assets/BloodAxes.png'
+import DeathSkulls from '../../assets/DeathSkulls.png'
+import EvilSunz from '../../assets/EviilSunz.png'
+import FreeBooterz from '../../assets/Freebooterz.png'
+import Goffs from '../../assets/Goffs.png'
+import SnakeBites from '../../assets/SakeBites.png'
+
+function WAAGH() {
+    return (
+        <div id='containerWAAGH'>
+            <section> <img src={KOPTERO} alt="" className='animation' /></section>
+            <main className='mainSectionOrkoz'>
+
+                <section id='orkozMaiinIfo'>
+                    <h2>LOS ORKOZ</h2>
+                    <div><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa dolore animi dolorum repellendus, quaerat, nihil perferendis nesciunt pariatur blanditiis quidem fugit saepe ad ea maiores dolorem, laborum impedit libero. Eligendi.
+                        Nostrum enim optio deleniti asperiores quidem doloribus neque quos. Ea reprehenderit explicabo magnam maiores earum assumenda maxime inventore sint modi eum, blanditiis doloribus ipsa aperiam consequuntur culpa quia consectetur quaerat.
+                        Facilis repudiandae provident nobis eaque alias voluptatem tenetur hic ipsum aspernatur doloremque voluptas corrupti libero, quam consequuntur. Natus alias quas asperiores, nam blanditiis accusamus. Non quaerat animi sapiente beatae cupiditate.
+                        Quasi nisi facilis enim dolorem harum tempore sit magni pariatur accusamus, tempora facere aliquam, fuga, iure odio? Unde doloribus perferendis autem quia, totam numquam, inventore ad laborum corrupti dignissimos debitis.
+                        A omnis veniam odit recusandae nulla temporibus illum itaque qui asperiores sapiente nam aspernatur, cupiditate natus accusamus provident? Atque pariatur voluptate veniam deleniti sapiente enim nemo accusantium quisquam neque? Nesciunt?
+                        Reiciendis atque, animi saepe laudantium pariatur sit possimus delectus ducimus provident illum laboriosam consequuntur minus voluptatem, repellat odio illo ullam aperiam dignissimos rem reprehenderit at harum ea aliquid! Accusantium, cumque?
+                        Provident necessitatibus a est vero tempora suscipit eveniet ratione at consequuntur, maiores maxime eaque dolorem quas perferendis iure saepe neque laboriosam officiis soluta minus. Culpa sit ipsa omnis ut deserunt?
+                        Mollitia dicta quidem, optio perferendis fugit, maiores, facere amet ab excepturi temporibus eaque inventore cum. Architecto minima ad quod, praesentium eum corporis. Ipsam quaerat exercitationem optio adipisci quisquam illo asperiores!
+                        In, sequi! Beatae praesentium nihil labore doloribus rem at unde nisi. Porro ratione tenetur exercitationem natus, ipsum vel omnis maxime dicta, corporis, soluta ipsa ea eos dolores ut consequatur possimus.
+                        Voluptatem, voluptates totam ipsam ab eius aliquam error incidunt, reiciendis blanditiis, voluptate laudantium voluptatum. Facere nulla doloribus, veritatis pariatur sapiente ipsum! Alias a excepturi, voluptates placeat dolorem saepe autem aperiam?
+                        Eveniet consectetur maiores magni non fugiat totam corporis placeat deserunt natus facilis, quam incidunt harum! Distinctio, ipsum esse illo recusandae voluptate magnam maiores accusamus perferendis sed! Culpa ab accusamus corporis!
+                        Quam omnis, optio deserunt quia aperiam tempore, voluptatibus nihil at dolor, porro perspiciatis dolores laudantium maxime rerum? Labore unde placeat, nesciunt officia ducimus rerum, eveniet quia ea quo aut quisquam!
+                        Saepe quam provident odio voluptatum? Nam optio ratione cum, sunt eligendi laboriosam magni culpa, voluptatibus incidunt voluptate, magnam beatae illo totam autem vel atque laudantium ex ab necessitatibus voluptatum ullam!
+                        Eligendi explicabo neque eaque doloribus voluptates veniam mollitia obcaecati quod ipsa, illum quo, corrupti quos, ducimus veritatis pariatur voluptas labore nam officia nobis laboriosam autem! Ullam delectus quisquam tempore ratione.
+                        atur nemo deleniti commodi, iste adipisci quisquam magni quia est voluptate accusamus excepturi nulla atque in magnam ipsa ut, cum modi asperiores id! Quia, amet ipsam.
+                        Repellat obcaecati sit dolorum enim quo ea quam, ipsa atque ad mollitia in, accusamus ut. Obcaecati omnis soluta itaque aspernatur nostrum a fugiat beatae accusantium, repudiandae molestias assumenda culpa nihil!
+                        Quos ab earum distinctio, nobis dolor perspiciatis eaque commodi aliquam amet nulla error quis, ratione, alias eum nostrum pariatur iste itaque sapiente laudantium aspernatur adipisci. Ut eveniet ipsa recusandae? Est.
+                        Impedit vero iste qui dolor ea! Ipsam temporibus illum praesentium nam eveniet reprehenderit! Aliquid nihil odit nesciunt, dolore mollitia dignissimos distinctio? Ipsum, deserunt doloribus! Iusto nesciunt tempore animi non repellat.
+                        Quibusdam quas, nulla perspiciatis, deserunt odio veritatis voluptas incidunt laudantium, ratione minus sit saepe. Aliquam autem repudiandae, ducimus aperiam adipisci, eaque, porro beatae aut molestiae ipsa dolore inventore consequuntur possimus?
+                        Iusto facere voluptatibus officiis quis. Reiciendis esse necessitatibus suscipit temporibus. Quo esse autem alias aperiam a dolor quas explicabo architecto recusandae ipsam possimus quia, optio consectetur atque deleniti corrupti. Provident?
+                        Reiciendis, exercitationem? Repudiandae molestias animi nostrum placeat, aliquam eveniet vitae at totam! Est saepe error, iure soluta quas voluptatibus aut aliquam quasi at, commodi laudantium perspiciatis officiis pariatur consequatur facere.
+                        Dicta, quidem? Ad porro iure enim. Nisi nobis magnam dignissimos corporis, cupiditate, accusamus voluptas amet incidunt eius debitis, pariatur ab cum itaque! Perferendis debitis, facilis repellat fugit dolorum saepe rem!
+                        Est harum qui quidem illum eius doloribus minus, voluptates maxime, hic unde fugit eveniet? Nesciunt corporis ipsam temporibus, officia sit aperiam minus nihil ad. Excepturi repudiandae cum labore mollitia nesciunt.
+                        Ratione pariatur reiciendis voluptatum magni aliquam aspernatur. Ducimus provident porro facere placeat rem vel? A adipisci libero quia error eaque, minima, rerum placeat corrupti doloremque, eum natus voluptate optio itaque?</p></div>
+                </section>
+                <section className='clanzSectiion'>
+                    <div className='cardClanzComponent'>
+                        <img src={BadMoons} alt="" className='clanzImgSize' />
+                        <h4>Bad Moons</h4>
+                    </div>
+                    <div className='cardClanzComponent'>
+                        <img src={BloodAxes} alt="" className='clanzImgSize' />
+                        <h4>Blood Axes</h4>
+                    </div>
+                    <div className='cardClanzComponent'>
+                        <img src={DeathSkulls} alt="" className='clanzImgSize' />
+                        <h4>Death Skulls</h4>
+                    </div>
+                    <div className='cardClanzComponent'>
+                        <img src={EvilSunz} alt="" className='clanzImgSize' />
+                        <h4>Evil Sunz</h4>
+                    </div>
+                    <div className='cardClanzComponent'>
+                        <img src={FreeBooterz} alt="" className='clanzImgSize' />
+                        <h4>FreeBooterz</h4>
+                    </div>
+                    <div className='cardClanzComponent'>
+                        <img src={Goffs} alt="" className='clanzImgSize' />
+                        <h4>Goffs</h4>
+                    </div>
+                    <div className='cardClanzComponent'>
+                        <img src={SnakeBites} alt="" className='clanzImgSize' />
+                        <h4>Snake Bites</h4>
+                    </div>
+                </section>
+            </main>
+
+        </div>
+    )
+}
+
+export default WAAGH
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,23 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from "./Components/HomePage/HomePage.jsx";
 import NavBar from "./Components/NavBar/NavBar.jsx";
+import WAAGH from "./Components/WAAGH/WAAGH.jsx";
 
 const Router = createBrowserRouter([
-  {path:"/", element: <HomePage/>}
+  { path: "/", element: <HomePage /> },
+  {
+    path: "/nav", element: <NavBar />,
+    children: [
+
+    ],
+  },
+  {
+    path: "waagh",
+    element: <WAAGH />
+  },
+
+
+
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
