62d07be1ab0f08a304729ba5b0facabdaa9b45ae
DiegoPerez1 <dieguitodieslow@gmail.com>
2023-05-28 12:39:50 -0300

    Merge

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -15,16 +15,70 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   const [show, setShow] = useState(false);
+  const [showLogin, setShowLogin] = useState(false)
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleCloseLogin = () => setShowLogin(false);
+  const handleShowLogin = () => setShowLogin(true);
+  const [loged, setLoged] = useState(false)
+  const [userInfo, setUserInfo] = useState([])
   const [signEmail, setSignEmail] = useState("");
   const [signValidEmail, setSignValidEmail] = useState(false);
-
+  const [validPassword, setValidPassword] = useState(false);
+  const [password, setPassword] = useState("");
 
   //Functions
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    localStorage.setItem('email', email)
+  const handleSubmit = async (event) => {
+
+    //Register Fetch
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const user = {
+      mail: signEmail,
+      password: password
+    };
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(user),
+      redirect: 'follow'
+    };
+
+    await fetch("http://localhost:8000/api/register", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  };
+
+  const handleSubmitLogin = () => {
+
+    //Login Fetch
+    const userlogin = { email: signEmail, password: password };
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify(userlogin),
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:8000/api/login", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.token) {
+          localStorage.setItem("token", result.token),
+            localStorage.setItem("iduser", result.id_user),
+            setLoged(true);
+
+        } else {
+          alert('Contraseña o Nombre Incorrecto')
+        }
+      })
+
+      .catch((error) => console.log("error", error));
   };
   const handleEmailChange = (event) => {
     const emailValue = event.target.value;
@@ -34,7 +88,35 @@ function NavBar() {
     setSignValidEmail(isValid);
   };
 
+  const handlePasswordChange = (event) => {
+    const passwordValue = event.target.value;
+    setPassword(passwordValue);
+    // Aca va la validacion de la base de datos de si el nombre de usuario esta disponible
+    let isValid = passwordValue.length >= 8;
+    setValidPassword(isValid);
+  }
+  const onModalClose = () => {
+    setSignEmail('')
+    setPassword('')
+  }
 
+  useEffect(() => {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch(`http://localhost:8000/api/userEmail?iduser=${localStorage.getItem("iduser")}`, requestOptions)
+      .then(response => response.json())
+      .then(result => setUserInfo(result.rslt[0]))
+      .catch(error => console.log('error', error));
+
+  }, []);
+
+  const handleLogOut = () => {
+    localStorage.clear()
+    setLoged(false)
+  }
 
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='navBarBar'>
@@ -54,19 +136,26 @@ function NavBar() {
             <Nav.Link href="#pricing" className='navBarItems'>World</Nav.Link>
             <Nav.Link href="#pricing" className='navBarItems'>About Us</Nav.Link>
           </Nav>
-          <Nav>
-            <Nav.Link href="#deets" className='navBarItems'><Button className='NavBarRegisterButton' variant="primary" onClick={handleShow}>
-              Register
-            </Button></Nav.Link>
-            <Nav.Link href="#deets" className='navBarItems'>Log In</Nav.Link>
-          </Nav>
+          {loged ?
+            <NavDropdown title={userInfo.email} id="collasible-nav-dropdown" className='userDropDown'>
+              <NavDropdown.Item href="#action/3.1" className='NavBarDropDownItem'>Profile</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3" className='NavBarDropDownItem' onClick={handleLogOut}>Log out</NavDropdown.Item>
+            </NavDropdown>
+            :
+            <Nav>
+              <Nav.Link href="#deets" className='navBarItems'><Button className='NavBarRegisterButton' variant="primary" onClick={handleShow}>
+                Register
+              </Button></Nav.Link>
+              <Nav.Link href="#deets" className='navBarItems'><Button className='NavBarRegisterButton' variant="primary" onClick={handleShowLogin}>Log In</Button></Nav.Link>
+            </Nav>
+          }
         </Navbar.Collapse>
       </Container>
 
       <Modal show={show} onHide={handleClose} className='signUpModal'>
         <Modal.Header closeButton>
           <img src={servoskull2} className='servoSignUp' />
-          <Modal.Title>Register</Modal.Title>
+          <Modal.Title style={{ fontFamily: "UnifrakturMaguntia", color: "white", fontWeight: "bolder" }} className='signUpTitle'>Join the Emperor!</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
@@ -82,23 +171,62 @@ function NavBar() {
               />
               <Form.Label>Password</Form.Label>
               <Form.Control
-                type="email"
+                type="password"
                 placeholder="************"
-                value={signEmail}
-                onChange={handleEmailChange}
+                value={password}
+                onChange={handlePasswordChange}
                 className="signInNavBarEmail"
-
               />
             </Form.Group>
             <p className="SignInEmailMessage">You'll need to confirm this information later on.</p>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={() => { handleClose(), onModalClose() }}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+          <Button className='signUpButton' variant="primary" onClick={() => { handleSubmit(), handleClose(), onModalClose() }} disabled={validPassword && signValidEmail ? false : true}>
+            Sign Up
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/*Here the Login Modal */}
+
+      <Modal show={showLogin} onHide={handleCloseLogin} className='signUpModal'>
+        <Modal.Header closeButton>
+          <img src={servoskull2} className='servoSignUp' />
+          <Modal.Title style={{ fontFamily: "UnifrakturMaguntia", color: "white", fontWeight: "bolder" }} className='signUpTitleLogin'>Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                value={signEmail}
+                onChange={handleEmailChange}
+                className="signInNavBarEmail"
+
+              />
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="************"
+                value={password}
+                onChange={handlePasswordChange}
+                className="signInNavBarEmail"
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => { handleCloseLogin(), onModalClose() }}>
+            Close
+          </Button>
+          <Button className='signUpButton' variant="primary" onClick={() => { handleSubmitLogin(), handleCloseLogin(), onModalClose() }} disabled={validPassword && signValidEmail ? false : true}>
+            Log In
           </Button>
         </Modal.Footer>
       </Modal>
